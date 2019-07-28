86ad536c8bc560bf5a6db8821137dfdb739503d3
Daluxolo Msikinya <business52@live.com>
2019-07-28 16:14:57 +0200

    initial commit

---
+++ aspnet-core/src/blogger.EntityFrameworkCore/EntityFrameworkCore/bloggerDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Abp.Zero.EntityFrameworkCore;
+using blogger.Authorization.Roles;
+using blogger.Authorization.Users;
+using blogger.MultiTenancy;
+
+namespace blogger.EntityFrameworkCore
+{
+    public class bloggerDbContext : AbpZeroDbContext<Tenant, Role, User, bloggerDbContext>
+    {
+        /* Define a DbSet for each entity of the application */
+        
+        public bloggerDbContext(DbContextOptions<bloggerDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
